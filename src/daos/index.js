f5eee68189f81c546e7fa3204acb03112b487ecd
Emiliar2002 <earancio616@gmail.com>
2022-12-08 23:44:11 -0300

    daos config for selected table

--- src/daos/index.js
+++ src/daos/index.js
@@ -1,22 +1,21 @@
 const fs = require('fs/promises')
 
-const getProductModule = async () => {
+const getModule = async (name) => {
     const dataCore = process.env.DATACORE
 
-    const productDir = await fs.readdir(__dirname + '/products')
-    console.log(productDir)
+    const dir = await fs.readdir(__dirname + '/' + name)
     let ModuleSource = await import(__dirname + '/products/memory.products.mjs')
-    productDir.forEach(async (i) => {
+    dir.forEach(async (i) => {
         const split = i.split('.')
-        if(split[0] === dataCore) ModuleSource = await import (__dirname + `/products/${i}`)
+        if(split[0] === dataCore) ModuleSource = await import (__dirname + `/${dir}/${i}`)
     })
     return ModuleSource.default
 }
 
-const ProductDaos = async () => {
-    const ProductDaosClass = await getProductModule();
-    const ProductDaosInstance = new ProductDaosClass()    
-    return ProductDaosInstance;
+const Daos = async (table) => {
+    const DaosClass = await getModule(table);
+    const DaosInstance = new DaosClass()    
+    return DaosInstance;
 }
 
-module.exports = ProductDaos
+module.exports = Daos
